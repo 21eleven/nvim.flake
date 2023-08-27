39422deb2b63c230434aa87091b3434b952ee742
Noah Lidell <noahlidell@gmail.com>
2023-08-27 11:50:01 -0500

    leap hotkeys

--- config/lua/leap.lua
+++ config/lua/leap.lua
@@ -1,4 +1,4 @@
-require("leap").add_default_mappings()
+-- require("leap").add_default_mappings()
 require('flit').setup {
   keys = { f = 'f', F = 'F', t = 't', T = 'T' },
   -- A string like "nv", "nvo", "o", etc.
@@ -12,4 +12,10 @@ require('flit').setup {
 -- leap-spooky for text objects?
 vim.keymap.set("n", "<leader>s", ":lua require('leap').leap { target_windows = { vim.fn.win_getid() } }<cr>")
 vim.keymap.set("n", "r", ":lua require('leap').leap { target_windows = { vim.fn.win_getid() } }<cr>")
-
+vim.keymap.set({'n', 'x', 'o'}, 'ss', 'xi')
+vim.keymap.set({'n', 'x', 'o'}, 'sk', '<Plug>(leap-forward-to)')
+vim.keymap.set({'n', 'x', 'o'}, 'sd', '<Plug>(leap-forward-to)')
+vim.keymap.set({'n', 'x', 'o'}, 'si', '<Plug>(leap-forward-till)')
+vim.keymap.set({'n', 'x', 'o'}, 'sj', '<Plug>(leap-backward-to)')
+vim.keymap.set({'n', 'x', 'o'}, 'sa', '<Plug>(leap-backward-to)')
+vim.keymap.set({'n', 'x', 'o'}, 'su', '<Plug>(leap-backward-till)')
