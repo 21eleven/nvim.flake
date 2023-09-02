955c837d5aafc7a31ec12ca38ef4be70df844943
Noah Lidell <noahlidell@gmail.com>
2023-09-02 12:47:30 -0500

    harpoon hotkeys

--- config/lua/harpoon.lua
+++ config/lua/harpoon.lua
@@ -5,3 +5,9 @@ local opt = { noremap = true }
 
 vim.api.nvim_set_keymap("n", "zh", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", opt)
 vim.api.nvim_set_keymap("n", "zm", "<cmd>lua require('harpoon.mark').add_file()<CR>", opt)
+vim.keymap.set("n", "zj", function () require("harpoon.ui").nav_file(1) end)
+vim.keymap.set("n", "zk", function () require("harpoon.ui").nav_file(2) end)
+vim.keymap.set("n", "zl", function () require("harpoon.ui").nav_file(3) end)
+vim.keymap.set("n", "<leader>j", function () require("harpoon.ui").nav_file(1) end)
+vim.keymap.set("n", "<leader>k", function () require("harpoon.ui").nav_file(2) end)
+vim.keymap.set("n", "<leader>l", function () require("harpoon.ui").nav_file(3) end)
